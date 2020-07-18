107c2a7529c00c207213cb7cf836c62337553c36
TSonono <tofiksonono@msn.com>
2020-07-18 10:17:30 +0200

    Modified setup.py to install requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as frq:
+    requirements = frq.read().splitlines()
+
 setuptools.setup(
     name="cryptography_bytes",
     version="0.0.1",
@@ -19,4 +22,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=requirements,
+
 )
